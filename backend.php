15a98aaa726e5807bebd4a3f3723ebd0b022dee4
Dario Baumberger <dario.baumberger@gmail.com>
2024-05-27 22:21:26 +0200

    wip

--- backend.php
+++ backend.php
@@ -2,53 +2,45 @@
 	header("Content-Type: application/json");
 	header("Access-Control-Allow-Methods: GET, POST");
 	header("Access-Control-Allow-Headers: Content-Type");
-	setcookie('test', '1', time() + (86400 * 30), "/");
 
 	class Database {
 		protected static $conn = null;
 
-		function __construct() {
+		public function __construct() {
 			if (self::$conn == null) {
 				self::$conn = new PDO("mysql:host=db;port=3306;dbname=webt", "root", "root");
 				self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
 		}
 
-		function randomID($length) {
+		private function randomID($length) {
 			$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 			return substr(str_shuffle($characters), 0, $length);
 		}
 
-		function readAllDocuments() {
+		public function get_all_entries() {
 			$sql = "SELECT * FROM test";
 			$stmt = self::$conn->prepare($sql);
 			$stmt->execute();
 			return $stmt->fetchAll(PDO::FETCH_ASSOC);
 		}
 
-		function readDocument($id) {
+		public function get_entry($id) {
 			$sql = "SELECT * FROM test WHERE place_id = ?";
 			$stmt = self::$conn->prepare($sql);
 			$stmt->execute([$id]);
 			return $stmt->fetch(PDO::FETCH_ASSOC);
 		}
 
-		function writeDocument($data) {
-			$place_id = self::randomID(11);
+		public function write_entry($data) {
+			$data['place_id'] = self::randomID(11);
 			$sql = 'INSERT INTO test (place_id, name, website, url, postal_code, locality, type) VALUES (:place_id, :name, :website, :url, :postal_code, :locality, :type)';
 			$stmt = self::$conn->prepare($sql);
-			$stmt->bindParam(':place_id', $place_id);
-			$stmt->bindParam(':name', $data['name']);
-			$stmt->bindParam(':website', $data['website']);
-			$stmt->bindParam(':url', $data['url']);
-			$stmt->bindParam(':postal_code', $data['postal_code']);
-			$stmt->bindParam(':locality', $data['locality']);
-			$stmt->bindParam(':type', $data['type']);
-			$stmt->execute();
-			return $place_id;
+			$stmt->execute($data);
+			return $data['place_id'];
 		}
 
-		function updateDocument($id, $data) {
+		public function update_entry($id, $data) {
 			$sql = 'UPDATE test SET name = :name WHERE place_id = :place_id';
 			$stmt = self::$conn->prepare($sql);
 			$stmt->bindParam(':place_id', $id);
@@ -57,31 +49,51 @@
 			return $stmt->rowCount() > 0;
 		}
 
-		function deleteDocument($id) {
+		public function delet_entry($id) {
 			$sql = 'DELETE FROM test WHERE place_id = :place_id';
 			$stmt = self::$conn->prepare($sql);
 			$stmt->bindParam(':place_id', $id);
 			$stmt->execute();
 			return $stmt->rowCount() > 0;
 		}
+
+		public function search_entries($params) {
+			$sql = "SELECT * FROM test WHERE ";
+			$values = [];
+			foreach ($params as $key => $value) {
+				$sql .= "$key = :$key AND ";
+				$values[":$key"] = $value;
+			}
+			$sql = rtrim($sql, " AND ");  // remove the trailing " AND "
+			$stmt = self::$conn->prepare($sql);
+			$stmt->execute($values);
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
 	}
 
 	class Backend {
 		protected $db;
+		protected $rules = ['name' => ['required' => true, 'minlength' => 1], 'postal_code' => ['required' => true, 'minlength' => 3, 'maxlength' => 6], 'locality' => ['required' => true, 'minlength' => 2], 'website' => ['required' => false, 'minlength' => 3, 'maxlength' => 100], 'url' => ['required' => false], 'place_id' => ['required' => false], 'type' => ['required' => true]];
 
 		function __construct() {
 			$this->db = new Database();
 			$this->handleRequest();
 		}
 
-		function handleRequest() {
+		private function handleRequest() {
 			$method = $_SERVER['REQUEST_METHOD'];
+			$params = $_GET;
 			$id = $_GET['id'] ?? null;
 			switch ($method) {
 				case 'GET':
 					if ($id) {
 						$this->handleGetSingleRequest($id);
-					} else {
+					} elseif (!empty($params)) {
+						$this->handleSearchRequest($params);
+					} /* elseif (isset($_COOKIE['search']) && !empty($_COOKIE['search'])) {
+					$searchParams = json_decode($_COOKIE['search'], true);
+					$this->handleSearchRequest($searchParams);
+				} */ else {
 						$this->handleGetAllRequest();
 					}
 					break;
@@ -99,18 +111,17 @@
 			}
 		}
 
-		function handleGetAllRequest() {
-			$documents = $this->db->readAllDocuments();
+		private function handleGetAllRequest() {
+			$documents = $this->db->get_all_entries();
 			if (empty($documents)) {
-				http_response_code(404);
-				echo json_encode(['error' => 'No documents found']);
+				echo json_encode([]);
 				return;
 			}
 			echo json_encode($documents);
 		}
 
-		function handleGetSingleRequest($id) {
-			$document = $this->db->readDocument($id);
+		private function handleGetSingleRequest($id) {
+			$document = $this->db->get_entry($id);
 			if (!$document) {
 				http_response_code(404);
 				echo json_encode(['error' => 'Document not found']);
@@ -119,17 +130,17 @@
 			echo json_encode($document);
 		}
 
-		function handlePostRequest() {
+		private function handlePostRequest() {
 			$body = file_get_contents("php://input");
 			$json = json_decode($body, true);
-			$id = $this->db->writeDocument($json);
-			echo json_encode(['id' => $id]);
+			self::validateData($json, $this->rules);
+			$this->db->write_entry($json);
 		}
 
-		function handlePutRequest($id) {
+		private function handlePutRequest($id) {
 			$body = file_get_contents("php://input");
 			$json = json_decode($body, true);
-			$updated = $this->db->updateDocument($id, $json);
+			$updated = $this->db->update_entry($id, $json);
 			if (!$updated) {
 				http_response_code(404);
 				echo json_encode(['error' => 'Document not found']);
@@ -138,14 +149,59 @@
 			echo json_encode(['success' => true]);
 		}
 
-		function handleDeleteRequest($id) {
-			$deleted = $this->db->deleteDocument($id);
+		private function handleDeleteRequest($id) {
+			$deleted = $this->db->delet_entry($id);
 			if (!$deleted) {
 				http_response_code(404);
 				echo json_encode(['error' => 'Document not found']);
 				return;
 			}
 			echo json_encode(['success' => true]);
+		}
+
+		private function handleSearchRequest($params) {
+			$cookie = [];
+			foreach ($params as $key => $value) {
+
+				$cookie["$key"] = $value;
+			}
+			setcookie('search', json_encode($cookie), time() + (3600 * 60 * 30), "/");
+
+			$documents = $this->db->search_entries($params);
+			if (empty($documents)) {
+				echo json_encode([]);
+				return;
+			}
+			echo json_encode($documents);
+		}
+
+		private function validateField($field, $rules) {
+			if ($rules['required'] && empty($field)) {
+				return 'This field is required.';
+			}
+
+			if (isset($rules['minlength']) && isset($field) && strlen($field) < $rules['minlength']) {
+				return 'This field must be at least ' . $rules['minlength'] . ' characters long.';
+			}
+
+			if (isset($rules['maxlength']) && isset($field) && strlen($field) > $rules['maxlength']) {
+				return 'This field must be no more than ' . $rules['maxlength'] . ' characters long.';
+			}
+
+			return '';
+		}
+
+		private function validateData($data, $rules) {
+			$errors = [];
+
+			foreach ($rules as $field => $rule) {
+				$error = $this->validateField($data[$field], $rule);
+				if ($error) {
+					$errors[$field] = $error;
+				}
+			}
+
+			return $errors;
 		}
 	}
 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 "use strict";
-
-const fetchOptions = {
+const fetchOptions = (method) => ({
 	mode: "cors",
 	cache: "no-cache",
 	credentials: "same-origin",
@@ -8,29 +7,28 @@ const fetchOptions = {
 		"Content-Type": "application/json"
 	},
 	redirect: "follow",
-	referrerPolicy: "no-referrer"
-};
+	referrerPolicy: "no-referrer",
+	method
+});
 
 async function submitEntries(data, url) {
 	const response = await fetch(url, {
-		...fetchOptions,
-		method: "POST",
+		...fetchOptions("POST"),
 		body: JSON.stringify(data)
 	});
 	return response;
 }
 
-async function loadEntries() {
-	console.log("load");
-	const response = await fetch("/api/test", {
-		...fetchOptions,
-		method: "GET"
-	});
-
+async function loadEntries(type = "", place = "") {
+	let queries = [];
+	if (type) queries.push(`type=${encodeURIComponent(type)}`);
+	if (place) queries.push(`locality=${encodeURIComponent(place)}`);
+	const queryString = queries.length ? `?${queries.join("&")}` : "";
+	const url = `/api/test${queryString}`;
+	const response = await fetch(url, fetchOptions("GET"));
 	if (!response.ok) {
 		throw new Error("Network response was not ok");
 	}
-
 	return response.json();
 }
 
@@ -50,7 +48,6 @@ function validateField(field) {
 			message: `Input is too long. Maximal length is ${field.maxlength}`
 		}
 	];
-
 	return rules.find((rule) => rule.condition)?.message || "";
 }
 
@@ -67,6 +64,10 @@ Vue.createApp({
 				place_id: createField(),
 				type: createField({ required: true })
 			},
+			search: {
+				place: "",
+				type: ""
+			},
 			result: "",
 			typeOptions: [
 				{ value: "", label: "Bitte wählen" },
@@ -79,55 +80,136 @@ Vue.createApp({
 	async created() {
 		try {
 			this.entries = await loadEntries();
+			this.localities = this.entries
+				.map((entry) => ({ locality: entry.locality, postal_code: entry.postal_code }))
+				.filter((value, index, self) => self.map((item) => item.locality).indexOf(value.locality) === index);
 		} catch (error) {
 			console.log("There has been a problem with your fetch operation: ", error.message);
 		}
 	},
+
 	mounted() {
 		const input = document.getElementById("search");
 		const autocomplete = new google.maps.places.Autocomplete(input);
-
 		autocomplete.setComponentRestrictions({ country: "ch" });
 		autocomplete.setTypes(["restaurant", "food"]);
-
 		autocomplete.addListener("place_changed", () => {
 			const place = autocomplete.getPlace();
 			const addressComponents = autocomplete.getPlace().address_components;
 			const placeFields = ["name", "place_id", "url", "website"];
 			const addressFields = ["locality", "postal_code"];
-
 			placeFields.forEach((field) => {
 				this.form[field].value = place[field];
 			});
-
 			addressFields.forEach((type) => {
 				this.form[type].value = addressComponents.find((component) => component.types.includes(type)).long_name;
 			});
 		});
+		const canvas = document.getElementById("canvas");
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+		canvas.width = width;
+		canvas.height = height;
+		const ctx = canvas.getContext("2d");
+
+		class Circle {
+			constructor(x, y, dx, dy, r, lineWidth) {
+				this.x = x;
+				this.y = y;
+				this.dx = dx;
+				this.dy = dy;
+				this.r = r;
+				this.lineWidth = lineWidth;
+				this.gradient;
+			}
+			draw = () => {
+				ctx.beginPath();
+				ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+				ctx.fillStyle = this.gradient;
+				ctx.strokeStyle = "rgba(255,255,255,0.2)";
+				ctx.lineWidth = this.lineWidth;
+				ctx.stroke();
+			};
+
+			update = () => {
+				this.dx = this.x + this.r > width || this.x - this.r < 0 ? -this.dx : this.dx;
+				this.dy = this.y + this.r > height || this.y - this.r < 0 ? -this.dy : this.dy;
+				this.x += this.dx;
+				this.y += this.dy;
+				this.draw();
+			};
+		}
+
+		const circles = [];
+		for (var i = 0; i < 10; i++) {
+			const radius = Math.floor(Math.random() * 120) + 50;
+			circles.push(
+				new Circle(
+					radius + Math.random() * width,
+					radius + Math.random() * height,
+					Math.floor(Math.random() * 2) + 1,
+					Math.floor(Math.random() * 2) + 1,
+					radius,
+					Math.floor(Math.random() * 5) + 3
+				)
+			);
+		}
+
+		function animate() {
+			requestAnimationFrame(animate);
+			ctx.clearRect(0, 0, width, height);
+			ctx.rect(0, 0, width, height);
+			ctx.fillStyle = "rgba(0,0,0,0.5)";
+			ctx.fill();
+			ctx.filter = "blur(5px)";
+			circles.forEach((circle) => circle.update());
+		}
+
+		animate();
 	},
 	methods: {
 		validate(key) {
 			const field = this.form[key];
 			const error = validateField(field);
 			field.error = error;
-
 			return !error;
 		},
-		async submit(e) {
-			e.preventDefault();
-			let validation = Object.keys(this.form).map((key) => validateField(key));
-
-			if (validation.some((value) => value === false)) {
-				return;
-			}
-
+		async submitSearch(e) {
 			try {
-				const result = (await submitEntries(this.form, "/api/test")).json();
-				this.resetForm();
-				result = `${result.name} wurde hinzugefügt`;
-				this.loadEntries();
+				this.entries = await loadEntries(this.search.type, this.search.place);
 			} catch (error) {
-				result = error;
+				console.log("There has been a problem with your fetch operation: ", error.message);
+			}
+		},
+
+		async resetSearch(e) {
+			this.search.place = "";
+			this.search.type = "";
+			document.cookie = "search=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+			try {
+				this.entries = await loadEntries(this.search.type, this.search.place);
+			} catch (error) {
+				console.log("There has been a problem with your fetch operation: ", error.message);
+			}
+		},
+
+		async submit() {
+			if (!Object.keys(this.form).every((key) => this.validate(key))) return;
+			try {
+				const formValues = Object.keys(this.form).reduce((obj, key) => {
+					obj[key] = this.form[key].value;
+					return obj;
+				}, {});
+				await submitEntries(formValues, "/api/test");
+				this.resetForm();
+				this.result = `${result.name} wurde hinzugefügt`;
+				try {
+					this.entries = await loadEntries();
+				} catch (error) {
+					console.log("There has been a problem with your fetch operation: ", error.message);
+				}
+			} catch (error) {
+				this.result = error;
 			}
 		},
 
